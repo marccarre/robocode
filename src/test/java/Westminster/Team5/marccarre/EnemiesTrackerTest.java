c3b75eee9880735b6066af42b7856bd3140b7261
Marc Carré <marccarre@users.noreply.github.com>
2015-10-06 20:13:53 +0100

    Updated tests according to previous commits.

--- src/test/java/Westminster/Team5/marccarre/EnemiesTrackerTest.java
+++ src/test/java/Westminster/Team5/marccarre/EnemiesTrackerTest.java
@@ -14,12 +14,12 @@ public class EnemiesTrackerTest {
 	public void enemiesTrackerShouldTrackEvents() {
 		EnemiesTracker tracker = new EnemiesTracker();
 		assertThat(tracker.size(), is(0));
-		assertThat(tracker.lastState(), is(nullValue()));
+		assertThat(tracker.latest(), is(nullValue()));
 
 		tracker.update(new ScannedRobotEvent("Robot#1", 1.0, 2.0, 3.0, 4.0, 5.0));
 		assertThat(tracker.size(), is(1));
-		assertThat(tracker.lastState(), is(not(nullValue())));
-		assertThat(tracker.lastState(), is(new State(1.0, 114.59155902616465, 3.0, 229.1831180523293, 5.0)));
+		assertThat(tracker.latest(), is(not(nullValue())));
+		assertThat(tracker.latest(), is(new State("Robot#1", 1.0, 114.59155902616465, 3.0, 229.1831180523293, 5.0)));
 	}
 
 	@Test
@@ -33,11 +33,11 @@ public class EnemiesTrackerTest {
 		tracker.update(new ScannedRobotEvent("Robot#1", 2.0, 3.0, 4.0, 5.0, 6.0));
 		tracker.update(new ScannedRobotEvent("Robot#1", 3.0, 4.0, 5.0, 6.0, 7.0));
 		assertThat(tracker.size(), is(stateHistoryMaxSize));
-		assertThat(tracker.lastState(), is(new State(3.0, 229.1831180523293, 5.0, 343.77467707849394, 7.0)));
+		assertThat(tracker.latest(), is(new State("Robot#1", 3.0, 229.1831180523293, 5.0, 343.77467707849394, 7.0)));
 
 		tracker.update(new ScannedRobotEvent("Robot#1", 4.0, 5.0, 6.0, 7.0, 8.0));
 		assertThat(tracker.size(), is(stateHistoryMaxSize)); // First event has been evicted.
-		assertThat(tracker.lastState(), is(new State(4.0, 286.4788975654116, 6.0, 401.07045659157626, 8.0)));
+		assertThat(tracker.latest(), is(new State("Robot#1", 4.0, 286.4788975654116, 6.0, 401.07045659157626, 8.0)));
 	}
 
 }
