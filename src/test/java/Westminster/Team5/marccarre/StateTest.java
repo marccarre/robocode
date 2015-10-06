957d7290d40e1e6b40bb10275671c2d2452f6056
Marc Carré <marccarre@users.noreply.github.com>
2015-10-06 18:03:40 +0100

    Updated tests according to previous commit.

--- src/test/java/Westminster/Team5/marccarre/StateTest.java
+++ src/test/java/Westminster/Team5/marccarre/StateTest.java
@@ -12,23 +12,23 @@ public class StateTest {
 
 	@Test
 	public void stateShouldHoldProvidedValues() {
-		final double bearing = 1.0;
-		final double distance = 2.0;
-		final double energy = 3.0;
+		final double energy = 1.0;
+		final double bearing = 2.0;
+		final double distance = 3.0;
 		final double heading = 4.0;
 		final double velocity = 5.0;
-		State state = new State(bearing, distance, energy, heading, velocity);
+		State state = new State(energy, bearing, distance, heading, velocity);
+		assertThat(state.energy(), is(energy));
 		assertThat(state.bearing(), is(bearing));
 		assertThat(state.distance(), is(distance));
-		assertThat(state.energy(), is(energy));
 		assertThat(state.heading(), is(heading));
 		assertThat(state.velocity(), is(velocity));
 	}
 
 	@Test
 	public void isCloserShouldSortByAscendingDistance() {
-		State x = new State(0, 100, 0, 0, 0);
-		State y = new State(0, 200, 0, 0, 0);
+		State x = new State(0, 0, 100, 0, 0);
+		State y = new State(0, 0, 200, 0, 0);
 		assertThat(State.IS_CLOSER.compare(x, y), is(lessThan(0)));
 
 		PriorityQueue<State> pq = new PriorityQueue<State>(State.IS_CLOSER);
@@ -39,8 +39,8 @@ public class StateTest {
 
 	@Test
 	public void isWeakerShouldSortByAscendingDistance() {
-		State x = new State(0, 0, 100, 0, 0);
-		State y = new State(0, 0, 200, 0, 0);
+		State x = new State(100, 0, 0, 0, 0);
+		State y = new State(200, 0, 0, 0, 0);
 		assertThat(State.IS_WEAKER.compare(x, y), is(lessThan(0)));
 
 		PriorityQueue<State> pq = new PriorityQueue<State>(State.IS_WEAKER);
