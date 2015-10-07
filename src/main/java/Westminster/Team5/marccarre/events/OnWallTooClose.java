a798bacb3cfaf180ffb79337218ae43eac39d13b
Marc Carré <marccarre@users.noreply.github.com>
2015-10-07 18:35:16 +0100

    Update OnWallTooClose.java

--- src/main/java/Westminster/Team5/marccarre/events/OnWallTooClose.java
+++ src/main/java/Westminster/Team5/marccarre/events/OnWallTooClose.java
@@ -4,35 +4,35 @@ import robocode.AdvancedRobot;
 import robocode.Condition;
 
 public class OnWallTooClose extends Condition {
-	public static final String EVENT_NAME = OnWallTooClose.class.getSimpleName();
+    public static final String EVENT_NAME = OnWallTooClose.class.getSimpleName();
 
-	private final AdvancedRobot robot;
-	private final double wallMargin;
+    private final AdvancedRobot robot;
+    private final double wallMargin;
 
-	public OnWallTooClose(final AdvancedRobot robot, final double wallMargin) {
-		super(EVENT_NAME);
-		this.robot = robot;
-		this.wallMargin = wallMargin;
-	}
+    public OnWallTooClose(final AdvancedRobot robot, final double wallMargin) {
+        super(EVENT_NAME);
+        this.robot = robot;
+        this.wallMargin = wallMargin;
+    }
 
-	@Override
-	public boolean test() {
-		return (leftWallTooClose() || rightWallTooClose() || topWallTooClose() || bottomWallTooClose());
-	}
+    @Override
+    public boolean test() {
+        return (leftWallTooClose() || rightWallTooClose() || topWallTooClose() || bottomWallTooClose());
+    }
 
-	private boolean leftWallTooClose() {
-		return (robot.getX() <= wallMargin);
-	}
+    private boolean leftWallTooClose() {
+        return (robot.getX() <= wallMargin);
+    }
 
-	private boolean rightWallTooClose() {
-		return (robot.getX() >= robot.getBattleFieldWidth() - wallMargin);
-	}
+    private boolean rightWallTooClose() {
+        return (robot.getX() >= robot.getBattleFieldWidth() - wallMargin);
+    }
 
-	private boolean topWallTooClose() {
-		return (robot.getY() >= robot.getBattleFieldHeight() - wallMargin);
-	}
+    private boolean topWallTooClose() {
+        return (robot.getY() >= robot.getBattleFieldHeight() - wallMargin);
+    }
 
-	private boolean bottomWallTooClose() {
-		return (robot.getY() <= wallMargin);
-	}
+    private boolean bottomWallTooClose() {
+        return (robot.getY() <= wallMargin);
+    }
 }
